0b9ab3560f9bee685c3cf73ad54b2355373b0d52
andre ferrer <andrerferrer@hotmail.com>
2021-05-12 06:42:00 -0300

    improve  JS

--- get_tas.js
+++ get_tas.js
@@ -1,2 +1,3 @@
 // go to the homepage and run
-[ ...document.querySelectorAll('.tas a') ].map( e => e.children[0].dataset.title )
+// https://kitt.lewagon.com/camps/999
+[ ...document.querySelectorAll('.tas a') ].map( e => e.children[0].dataset.title ).join(',')
