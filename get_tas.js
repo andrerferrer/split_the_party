791821ee440c0b1c01f977d05b29fda43c4cc8b8
Andre R Ferrer <45776359+andrerferrer@users.noreply.github.com>
2022-04-19 14:25:53 -0300

    Update get_tas.js

--- get_tas.js
+++ get_tas.js
@@ -1,2 +1,2 @@
-// go to the https://kitt.lewagon.com/camps/999 (replacing 999 with the batch number) and run
+// go to the https://kitt.lewagon.com/camps/999 (replacing 999 with the batch number) and run the following script in the console of the browser
 [ ...document.querySelectorAll('.dashboard-session-today-staff a') ].map( e => e.children[0].dataset.title ).join(',')
